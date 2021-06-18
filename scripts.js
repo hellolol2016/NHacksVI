f887a3ad19f4b39946ee6932bda6287d33f4b657
hellolol2016 <dennisthederp812@gmail.com>
2021-06-18 12:48:53 -0700

    imported firebase

--- scripts.js
+++ scripts.js
@@ -6,3 +6,20 @@ function initMap() {
     zoom: 8,
   });
 }
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBC1wJWfMgKxG99mUxn4YHkQVS_-MnBx_0",
+    authDomain: "recy-dc387.firebaseapp.com",
+    projectId: "recy-dc387",
+    storageBucket: "recy-dc387.appspot.com",
+    messagingSenderId: "205601819575",
+    appId: "1:205601819575:web:a0f5e587d1036ccd073632",
+    measurementId: "G-RL5PSZZ9CL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+let database = firebase.database();
